2af951d913ed71a350000b7949293110c09d7c41
Railson Cosmo <railson_sousa10@icloud.com>
2025-07-06 18:38:39 -0300

    feat: adiciona componente de TableTasks dinâmica

--- frontend/src/components/TableTasks.tsx
+++ frontend/src/components/TableTasks.tsx
@@ -1,27 +1,55 @@
-import useTaks from '../hooks/useTasks';
+import { useTasks } from '../hooks/useTasks';
+import { MdDelete } from "react-icons/md";
+import { FaEdit } from "react-icons/fa";
 
 export const TableTasks = () => {
-  const { tasks } = useTaks();
+  const { tasks, deleteTask, toggleTaskStatus } = useTasks();
 
   return (
     <table className="min-w-full border-collapse border border-gray-300">
   <thead>
     <tr className="bg-gray-100">
-      <th className="border border-gray-300 px-4 py-2 text-left">ID</th>
-      <th className="border border-gray-300 px-4 py-2 text-left">Descrição</th>
-      <th className="border border-gray-300 px-4 py-2 text-left">Concluída</th>
+      <th className="border border-gray-300 px-4 py-2 text-center">Imagem</th>
+      <th className="border border-gray-300 px-4 py-2 text-center">Descrição</th>
+      <th className="border border-gray-300 px-4 py-2 text-center">Concluída</th>
+      <th className="border border-gray-300 px-4 py-2 text-center">Ações</th>
     </tr>
   </thead>
   <tbody>
     {(tasks ?? []).map((task) => (
         <tr key={task.id}>
-          <td className="border text- px-4 py-2">{task.id}</td>
+          <td className="border text-center px-4 py-2">
+            {task.url_image ? (
+            <img
+              src={`http://localhost:3333/files/${task.url_image}`}
+              alt="tarefa"
+              className="w-20 h-20 object-cover rounded flex items-center"
+            />
+              ) : (
+            <span>Sem imagem</span>
+              )}
+          </td>
           <td className="border px-4 py-2">{task.description}</td>
-          <td className="border px-4 py-2"><input
+          <td className="border px-4 py-2 text-center">
+            <input
+            className='w-5 h-5 accent-blue-500'
             type="checkbox"
             checked={task.isCompleted}
-            // onChange={() => handleToggle(task.id)}
-          /></td>
+            onChange={() => toggleTaskStatus(task.id, task.isCompleted)}
+            />
+            </td>
+          <td className="border px-4 py-2 text-center">
+            <div className='flex gap-5 justify-center'>
+              <button
+                onClick={() => deleteTask(task.id)}
+              >
+                <MdDelete size={25} color='#8c161d'/>
+              </button>
+              <button>
+                <FaEdit size={25}/>
+              </button>
+            </div>
+          </td>
         </tr>
       ))}
   </tbody>
