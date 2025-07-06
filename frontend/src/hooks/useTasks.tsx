4183f4d35314fcf642e7b1867d8ab571476ebea3
Railson Cosmo <railson_sousa10@icloud.com>
2025-07-06 18:20:47 -0300

    feat: adiciona hook customizado

--- frontend/src/hooks/useTasks.tsx
+++ frontend/src/hooks/useTasks.tsx
@@ -2,8 +2,8 @@ import { useState, useEffect } from 'react';
 import { Task } from '../types/task';
 import { TaskService } from '../services/task.service';
 
-const useTaks = () => {
-  const [tasks, setTasks] = useState<Task[]>([])
+const useTasks = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,11 +18,50 @@ const useTaks = () => {
     }
 
     fetchData();
-  }, [])
+  }, [tasks])
+
+  const createNewTask = async (task: string, imageTask?: File) => {
+    if(!task){
+      return;
+    }
+    try {
+      const newTask = await TaskService.create(task, imageTask)
+      setTasks(prev => [...prev, newTask])
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const deleteTask = async(id: string) => {
+    await TaskService.delete(id);
+    setTasks(prev => prev.filter(task => task.id !== id));
+  }
+
+  const sortTasks = () => {
+    const sorted = [...tasks].sort((a, b) => Number(a.isCompleted) - Number(b.isCompleted));
+    setTasks(sorted);
+  };
+
+  const toggleTaskStatus = async (id: string, status: boolean) => {
+  try {
+    const updatedTask = await TaskService.update(id, status);
+    setTasks(prev =>
+      prev.map(task =>
+        task.id === id ? { ...task, isCompleted: updatedTask.isCompleted } : task
+      )
+    );
+  } catch (erro) {
+    console.error(erro);
+  }
+};
 
   return {
-    tasks
+    tasks,
+    createNewTask,
+    deleteTask,
+    sortTasks,
+    toggleTaskStatus
   }
 }
 
-export default useTaks;
+export { useTasks };
