8ab6ab810382804cc2bc263c32ac7099657bbe37
Railson Cosmo <railson_sousa10@icloud.com>
2025-07-06 09:43:26 -0300

    Implementa a classe TodoController para requisições

---
+++ src/controllers/todo/todo.controller.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from 'express';
+import { TodoService } from '../../services/todo/todo.service';
+import { CreateTodo, UpdateTodo } from '../../types/todo/todo.types';
+
+const todoService = new TodoService();
+
+export class TodoController {
+  async createNewTodo(req: Request, res: Response) {
+    const newTodo = req.body as CreateTodo;
+    await todoService.createTodo(newTodo);
+
+    return res.status(201).json(newTodo);
+  }
+
+  async getAllTodos(_req: Request, res: Response) {
+    const allTodos = await todoService.findAllTodos();
+
+    return res.status(200).json(allTodos);
+  }
+
+  async updateTodo(req: Request, res: Response) {
+    const { description, isCompleted, url_image } = req.body as UpdateTodo;
+    const { id } = req.params;
+
+    const todoUpdated = await todoService.updateTodo({
+      id,
+      description,
+      isCompleted,
+      url_image,
+    });
+
+    return res.status(200).json(todoUpdated);
+  }
+
+  async deleteTodo(req: Request, res: Response) {
+    const { id } = req.params;
+
+    await todoService.deleteTodo(id);
+
+    return res.status(204).send();
+  }
+}
